bb169dd18f6e9add81deaf9d0520815c8a0f9466
zhengyu901030 <zhou901030@gmail.com>
2017-11-28 22:40:49 -0500

    constructor stock class

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -13,3 +13,18 @@ export class ContentComponent implements OnInit {
   }
 
 }
+
+export class Stock {
+  constructor(
+    public id: number,
+    public name: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categories: Array<string>
+  )
+    {
+
+    }
+  
+}
