b6dd2cd493afd4c04a6c5393b63479a27a04a55c
zhengyu901030 <zhou901030@gmail.com>
2017-11-28 22:54:11 -0500

    mock data from stock info

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
+  private stocks:Array<Stock>;
+
   constructor() { }
 
   ngOnInit() {
+    this.stocks = [
+      new Stock(1,"FirstStockName",1.99,3.5,"this is first stock,can be any information in here, can be awesome",['IT','Internet']),
+      new Stock(2,"SecondStockName",2.99,1.5,"this is second stock,can be any information in here, can be awesome",['Education','Internet','IT']),
+      new Stock(3,"ThirdStockName",2.39,3,"this is third stock,can be any information in here, can be awesome",['IT','Media','Government']),
+      new Stock(4,"FouthStockName",3.19,4.5,"this is Fouth stock,can be any information in here, can be awesome",['Energy','Tech']),
+      new Stock(5,"FifthStockName",7.99,5,"this is Five stock,can be any information in here, can be awesome",['Power','Tech','Government']),
+      new Stock(6,"SixthStockName",1.39,1.5,"this is Sixth stock,can be any information in here, can be awesome",['E-Commerce','Internet','IT']),
+      new Stock(7,"SeventhStockName",5.99,2.5,"this is Seventh stock,can be any information in here, can be awesome",['Financial']),
+      new Stock(8,"EigthStockName",4.19,4,"this is Eighth stock,can be any information in here, can be awesome",['Financial','E-Commerce']),
+      
+    ];
   }
 
 }
