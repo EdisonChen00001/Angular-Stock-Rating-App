67d6bdca70841e490a6d623396a6142ccbc8573b
zhouH0901 <zhezhou@my.bridgeport.edu>
2017-12-28 20:30:22 -0500

    create manus list

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,12 +8,27 @@ import { Router } from "@angular/router";
 })
 export class MenuComponent implements OnInit {
 
+  menus:Array<Menu>;
+
   constructor(public router:Router) { }
 
   ngOnInit() {
+    this.menus = [
+      new Menu(1,'index','dashborad'),
+      new Menu(2,'stock manage','stock')
+    ];
   }
 
   nav(url:string){
     this.router.navigateByUrl(url);
+  }
+}
+
+export  class Menu{
+  constructor(
+    public id:number,
+    public name:string,
+    public link:string){
+
   }
 }
