4af1ad1bc1393571aafbc6ccec8e75b36648b285
zhouH0901 <zhezhou@my.bridgeport.edu>
2017-12-28 21:00:51 -0500

    add event router

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/filter'
+import { NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-content',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
+  pageTitle = '';
+  pageDesc = '';
 
-
-  constructor() { }
+  constructor(public router:Router) { 
+    router.events
+    .filter(event => event instanceof NavigationEnd)
+    .subscribe((event:NavigationEnd) => {
+      if(event.url == '/dashboard'){
+        this.pageTitle = "index page"
+        this.pageDesc = "this is index page, main content"
+      }else if (event.url.startsWith('/stock')){
+        this.pageTitle = "stock manage"
+        this.pageDesc = "this is the page for stocks management"
+      }
+    })
+  }
 
   ngOnInit() {
    
